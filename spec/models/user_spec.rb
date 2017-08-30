db3757fa63d78dc61e54b463dc4a07d3f39a5dd3
Wellington Santos <wlss.lucas@gmail.com>
2017-08-30 20:49:15 -0300

    added new tests for user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # assim, o user só é criado quando for chamado
+  # porém, não posso usar o should no caso de 'be_valid'
+  # pois ele tenta usar o subject, que só cahama o método
+  # 'new' do model
+  # let(:user) { build :user }
+
   subject { build :user }
 
   it { should respond_to :email }
@@ -9,5 +15,6 @@ RSpec.describe User, type: :model do
 
   it { should be_valid}
 
-  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_confirmation_of :password }
 end
