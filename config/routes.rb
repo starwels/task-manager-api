c5d6a9c882b666e4139fd80ce833e4ffbc460a45
Wellington Santos <wlss.lucas@gmail.com>
2017-08-30 19:28:00 -0300

    added version manager for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    namespace :v1, constraints: ApiVersionConstraint.new(version: 1, default: true), path: '/' do
+    end
+  end
 end
