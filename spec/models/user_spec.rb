3cff666fe5bf3cbd79567d33bfc60622547b95b8
Wellington Santos <wlss.lucas@gmail.com>
2017-08-30 19:53:23 -0300

    first test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build :user }
+
+  it { should respond_to :email }
+  it { should respond_to :password }
+  it { should respond_to :password_confirmation }
+
+  it { expect(@user).to be_valid}
+
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
 end
