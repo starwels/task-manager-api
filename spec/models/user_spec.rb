67c79509828be21275326740f4fb5c719c3b501e
Wellington Santos <wlss.lucas@gmail.com>
2017-08-30 20:31:51 -0300

    refactor of user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.build :user }
+  subject { build :user }
 
   it { should respond_to :email }
   it { should respond_to :password }
   it { should respond_to :password_confirmation }
 
-  it { expect(@user).to be_valid}
+  it { should be_valid}
 
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
 end
