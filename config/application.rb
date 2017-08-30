cf4d68f23a0e1c27798c5cab6e2bc750a685d439
Wellington Santos <wlss.lucas@gmail.com>
2017-08-30 20:21:08 -0300

    config for rspec

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module TaskManagerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.controller_specs false
+    end
   end
 end
