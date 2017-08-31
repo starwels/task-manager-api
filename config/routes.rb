6b73a7d7f16f7f71014dfcfe71a88c86540cecfc
Wellington Santos <wlss.lucas@gmail.com>
2017-08-31 01:37:00 -0300

    added destroy action and request tests

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     namespace :v1, constraints: ApiVersionConstraint.new(version: 1, default: true), path: '/' do
-      resources :users, only: [:show, :create, :update]
+      resources :users, only: [:show, :create, :update, :destroy]
     end
   end
 end
