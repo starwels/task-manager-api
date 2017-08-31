280d902a056b8fc88ec00f76b26371d3ec4b61a0
Wellington Santos <wlss.lucas@gmail.com>
2017-08-31 01:34:53 -0300

    added action update and request tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -96,4 +96,20 @@ RSpec.describe 'Users API', type: :request do
         end
     
     end
+
+    describe 'DELETE /users/:id' do
+        before do
+            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+            delete "/users/#{user_id}", params: {}, headers: headers
+        end
+
+        it 'returns status 204' do
+            expect(response).to have_http_status(204)
+        end
+
+        it 'removes user from db' do
+            expect(User.find_by(id: user_id)).to be_nil
+        end
+    end
+
 end
