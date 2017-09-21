fb1dfc695f7f6e74f3fff910630e35ad8c86c250
Wellington Santos <wlss.lucas@gmail.com>
2017-09-20 21:05:11 -0300

    removed redundant validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-  validates :email, uniqueness: true
 end
