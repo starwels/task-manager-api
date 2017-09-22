ad34688475534747946612aef150e0380483612a
Wellington Santos <wlss.lucas@gmail.com>
2017-09-21 22:50:00 -0300

    added auth token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_create :generate_authentication_token!
          
   validates :auth_token, uniqueness: true
 
   def info
     "#{email} - #{created_at} - #{Devise.friendly_token}"
+  end
+
+  def generate_authentication_token!
+    loop do
+      self.auth_token = Devise.friendly_token
+      break unless User.exists?(auth_token: auth_token)
+    end
   end
 end
